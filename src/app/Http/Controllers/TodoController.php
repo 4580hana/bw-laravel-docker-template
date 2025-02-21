0378625c9d71e0b57ca6f2f71295cc99c5c640fa
hana-4580 <hana-4580@hana-4580noMacBook-Pro.local>
2025-02-21 16:23:23 +0900

    section8

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -1,13 +1,28 @@
 <?php
-
+// ToDo一覧表示機能
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        dd('Hello World!');
+        $todo = new Todo();
+        $todos = $todo->all();
+
+        return view('todo.index', ['todos' => $todos]);
+      
+
     }
+    public function create()
+{
+      
+       return view('todo.create'); // 追記
+
 }
+
+}
+
+
